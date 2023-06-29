1d0596a29b3bedf60348cd7ad46a14214312a42f
ipaulbogdan <paul.idorasi@tesco.com>
2023-06-29 12:57:37 +0300

    Camel-Hub: Add stop script

---
+++ stop.sh
@@ -0,0 +1,3 @@
+docker stop $(docker ps | grep rest-producer | cut -d' ' -f1)
+docker stop $(docker ps | grep soap-producer | cut -d' ' -f1)
+docker stop $(docker ps | grep camel-server | cut -d' ' -f1)
